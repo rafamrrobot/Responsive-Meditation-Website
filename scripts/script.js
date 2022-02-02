dd98074135101f3ba9571e44603735e2147fa6b2
rafamrrobot <60102503+rafamrrobot@users.noreply.github.com>
2022-02-01 21:58:50 -0300

    Create script.js

---
+++ scripts/script.js
@@ -0,0 +1,50 @@
+/*Toogle sidemenu*/
+let navBar = document.querySelector("nav");
+let myNav = document.getElementById("nav-bar");
+let navLinks = myNav.querySelectorAll("li");
+
+let hamMenu = document.getElementById("ham-menu")
+hamMenu.addEventListener("click" ,() => {
+    myNav.classList.toggle("active");
+    hamMenu.classList.toggle("fa-times");
+});
+
+navLinks.forEach((navLink) => {
+    navLink.addEventListener("click" , ()=> {
+        myNav.classList.remove("active") ;
+        hamMenu.classList.toggle("fa-times")
+    })
+
+})
+
+window.onscroll = function () {
+    /*Scroll indicator*/
+let pos = document.documentElement.scrollTop ;
+let calcHeight = 
+    document.documentElement.scrollHeight - document.documentElement.clientHeight ;
+let scroll = (pos * 100) /calcHeight ;
+document.getElementById("progress").style.width = `${scroll}%` ;
+
+
+   /*Scroll to top button */
+   let scrollTopButton = document.getElementById("scroll-top-btn");
+     if (pos > 300) {
+         scrollTopButton.style.display = "grid";
+     }
+     else{
+         scrollTopButton.style.display ="none"
+     }
+   
+
+     scrollTopButton.addEventListener("click" , () => {
+         document.documentElement.scrollTop = 0
+     })
+     
+    /*Sticky Navbar*/
+    if (pos > 0) {
+        navBar.classList.add("sticky")
+    }else {
+        navBar.classList.remove("sticky")
+    }
+
+    }
